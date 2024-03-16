05eec37a17a0a27974521cc5b44aa698577a4626
Michele Silva <michelesilvasr1@gmail.com>
2024-03-16 19:56:09 -0300

    teste

---
+++ script.js
@@ -0,0 +1,80 @@
+//1
+//let INDICE = 13;
+//let SOMA = 0;
+//let K = 0;
+
+//while (K < INDICE) {
+ //   K = K + 1;
+ //   SOMA = SOMA + K;
+//}
+
+//console.log(SOMA);
+
+
+//2
+/*function verificaFibonacci(numero) {
+    let a = 0;
+    let b = 1;
+    
+    if (numero === a || numero === b) {
+        return true;
+    }
+    
+    while (b <= numero) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+        
+        if (b === numero) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+
+let numero = prompt("insira um numero para saber se ele pertence a sequência de fobonacci:")
+if (verificaFibonacci(numero)) {
+    console.log(numero + " pertence à sequência de Fibonacci.");
+} else {
+    console.log(numero + " não pertence à sequência de Fibonacci.");
+}
+*/
+
+//3
+/*a) 1, 3, 5, 7, _
+   - adicionar 2 ao número anterior.
+   - O próximo número seria 7 + 2 = 9.
+
+b) 2, 4, 8, 16, 32, 64, _
+   - multiplicar o número anterior por 2.
+   - O próximo número seria 64 * 2 = 128.
+
+c) 0, 1, 4, 9, 16, 25, 36, _
+   - elevar o número anterior ao quadrado.
+   - O próximo número seria 36 * 36 = 1296.
+
+d) 4, 16, 36, 64, _
+   - números são os quadrados dos números pares.
+   - O próximo número seria 100 , que é o quadrado de 10
+e) 1, 1, 2, 3, 5, 8, _
+   - sequência de Fibonacci.
+   - O próximo número seria a soma dos dois números anteriores, 5 + 8 = 13.
+
+f) 2, 10, 12, 16, 17, 18, 19, _
+   - essa sequencia contém os numeros que iniciam com a letra D, o numero seria 200
+   */
+
+   
+//4
+//Eu ligaria um dos interruptores por alguns minutos, depois voltaria para ligar o proximo e passando pelas salas veria qual luz estava ligada e qual estava a pouco tempo por ainda estar quente 
+
+//5
+/*let texto = "oi eu sou michele";
+let textoInvertido = inverterString(texto);
+console.log("Texto original:", texto);
+console.log("Texto invertido:", textoInvertido);
+*/
+
+
